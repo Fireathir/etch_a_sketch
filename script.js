b0e50468e40aa6718eb7fe74245bda4c6341d539
Jimmy Brandt <jimmy_brandt@hotmail.com>
2023-02-22 01:46:24 +0100

    A simple etch-a-sketch, might come back to this to make improve it

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+const gridSize = 16*16;
+const gridContainer = document.querySelector('#gameGrid');
+const resetButton = document.querySelector('#resetButton');
+
+for (let i = 0; i < gridSize; i++){
+    const row = document.createElement('div');
+    row.addEventListener('mouseover', function(event){
+        event.target.style.backgroundColor = 'black';
+    })
+    row.classList.add('row');
+    gridContainer.append(row);
+}
+
+let rows = document.querySelectorAll('.row');
+
+resetButton.addEventListener("click", clickFunction);
+
+function clickFunction(){
+    rows.forEach(row => {
+        row.style.backgroundColor = 'white';
+    });
+}
